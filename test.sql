f001bc9e0f816347d3b7785c02ca996ffe5d262c
Lê Minh Phong <43315085+windhx@users.noreply.github.com>
2018-11-23 11:23:59 +0700

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `test`.`employee` (
+    `employeeId` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(45) NULL,
+    `email` VARCHAR(45) NULL,
+    `designation` VARCHAR(45) NULL,
+    PRIMARY KEY (`employeeId`)
+);
+
